db461313980fd93975976cf9b1457543747d3f9a
khanhney <khanhney.dev@gmail.com>
2019-08-02 14:14:54 +0700

    final project with build mern heroku cloud

---
+++ public/precache-manifest.30982f2fd7e8b5a5b77bb262796aaca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684a87242086f4ae55c5c3fac3b812f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90f42ea1ffd4d209f780",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "adf402783834cf8f8009",
+    "url": "/static/js/2.d442b52c.chunk.js"
+  },
+  {
+    "revision": "90f42ea1ffd4d209f780",
+    "url": "/static/js/main.92ea90d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ public/precache-manifest.e71a9435faded06ded805f9753dfe84a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4187ae40b74a7834fdbe4a5ad3b338a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aabe7ee722df9262f33",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "adf402783834cf8f8009",
+    "url": "/static/js/2.d442b52c.chunk.js"
+  },
+  {
+    "revision": "8aabe7ee722df9262f33",
+    "url": "/static/js/main.8312bc3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
